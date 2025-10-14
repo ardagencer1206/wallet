3dcbbb2629198e5cfc6d7518d5db4ea16564340b
ardagencer1206 <73840764+ardagencer1206@users.noreply.github.com>
2025-10-14 11:42:41 +0300

    Update models.py

--- models.py
+++ models.py
@@ -14,3 +14,16 @@ class User(UserMixin, db.Model):
 
     def check_password(self, raw_password):
         return check_password_hash(self.password_hash, raw_password)
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    balance = db.Column(db.Numeric(precision=18, scale=2), default=0.00, nullable=False)  # SRDS bakiyesi
+
+    def set_password(self, raw_password):
+        self.password_hash = generate_password_hash(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password_hash(self.password_hash, raw_password)
