b9a61ec833f576e25055ea48fd36a5efb16ee4f2
ardagencer1206 <73840764+ardagencer1206@users.noreply.github.com>
2025-10-14 13:22:24 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms import StringField, PasswordField, SubmitField, DecimalField
+from wtforms.validators import DataRequired, Email, Length, NumberRange
 
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
@@ -11,3 +11,7 @@ class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Şifre", validators=[DataRequired()])
     submit = SubmitField("Giriş Yap")
+class TransferForm(FlaskForm):
+    to_email = StringField("Alıcı Email", validators=[DataRequired(), Email()])
+    amount = DecimalField("Miktar (SRDS)", places=2, validators=[DataRequired(), NumberRange(min=0.01)])
+    submit = SubmitField("Gönder")
