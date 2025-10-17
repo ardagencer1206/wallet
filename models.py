d6433620ff09b8bdd30b2fa977316fcca9c6903c
ardagencer1206 <73840764+ardagencer1206@users.noreply.github.com>
2025-10-17 15:24:10 +0300

    Update models.py

--- models.py
+++ models.py
@@ -23,13 +23,18 @@ class CommissionPool(db.Model):
     total = db.Column(db.Numeric(18, 2), nullable=False, server_default="0.00")
 
 
+class CirculatingSupply(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    total = db.Column(db.Numeric(20, 2), nullable=False, server_default="0.00")
+
+
 class TransferHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sender_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     receiver_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     amount = db.Column(db.Numeric(18, 2), nullable=False)
     commission = db.Column(db.Numeric(18, 2), nullable=False)
-    message = db.Column(db.String(500), nullable=True)  # eklendi
+    message = db.Column(db.String(500), nullable=True)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
 
 
