727423f6049d3664e53b6f41f0cc2224a2f9b4f3
ardagencer1206 <73840764+ardagencer1206@users.noreply.github.com>
2025-10-21 14:18:13 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,27 +1,30 @@
+# forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, DecimalField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length, Optional, NumberRange
-
 
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Şifre", validators=[DataRequired(), Length(min=6)])
     submit = SubmitField("Kayıt Ol")
 
-
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Şifre", validators=[DataRequired()])
     submit = SubmitField("Giriş Yap")
-
 
 class TransferForm(FlaskForm):
     to_email = StringField("Alıcı Email", validators=[DataRequired(), Email()])
     amount = DecimalField("Miktar (SRDS)", places=2, validators=[DataRequired(), NumberRange(min=0.01)])
     message = TextAreaField("Mesaj", validators=[Optional()])
     submit = SubmitField("Gönder")
-# ek: en alta koy
+
 class BuyForm(FlaskForm):
     amount = DecimalField("Alınacak SRDS miktarı", places=2,
                           validators=[DataRequired(), NumberRange(min=0.01)])
     submit = SubmitField("Satın Al")
+
+class SellForm(FlaskForm):
+    amount = DecimalField("Satılacak SRDS miktarı", places=2,
+                          validators=[DataRequired(), NumberRange(min=0.01)])
+    submit = SubmitField("Sat")
